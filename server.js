5a610fb34f53493d49550e66600318db929e1adb
Martin Aarhus Gregersen <marting12@live.dk>
2020-06-08 14:28:38 +0200

    Updates server script

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const http = require('http');
+const debug = require('debug')('node-angular');
 const app = require('./backend/app');
 
-const port = process.env.PORT || 3000;
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-// ES6 arrow function that we set to take 2 inputs, the request and response objects
-app.set('port', port);
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated priviledges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
 const server = http.createServer(app);
-
-// We default to the port given to us by the host environment, OR default to 3000 if no environment variable is found.
+server.on("error", onError);
+server.on("listening", onListening);
 server.listen(port);
