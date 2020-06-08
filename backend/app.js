c54213d043e34e4c6c2bde0f1577e395e2f8d48f
Martin Aarhus Gregersen <marting12@live.dk>
2020-06-08 22:51:29 +0200

    Creates real endpoint that exposes posts for angular

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-})
-
-app.use((req, res, next) => {
-  res.send('Hello from Express!');
+app.use('/api/posts', (req, res, next) => {
+  posts = [
+    { id: 'abc8172', title: 'First server side post', content: 'This text is coming from the server' },
+    { id: 'def12321', title: 'Second server side post!', content: 'This text is also coming from the server' }
+  ];
+  res.status(200).json({
+    message: "Posts fetched succesfully!",
+    posts: posts
+  });
 });
 
 // Exports the express server and all attached middlewares to use in another file
