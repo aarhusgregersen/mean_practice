676069050d1713ffdfdd48442705345241561c05
Martin Aarhus Gregersen <marting12@live.dk>
2020-06-10 22:18:35 +0200

    Adds bodyparser and post route for posts

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+// We currently dont have URL encoded data
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,7 +14,16 @@ app.use((req, res, next) => {
   next(); // Always call next on middleware you want to continue the request after
 });
 
-app.use('/api/posts', (req, res, next) => {
+// TODO: Add MongoDB so we can actually store the data we want to save.
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body();
+  console.log(post);
+  res.status(201).json({
+    message: 'Post Added succesfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   posts = [
     { id: 'abc8172', title: 'First server side post', content: 'This text is coming from the server' },
     { id: 'def12321', title: 'Second server side post!', content: 'This text is also coming from the server' }
