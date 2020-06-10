552f7209bf5e2391260dec6a1bbaf4b69e6dfad3
Martin Aarhus Gregersen <marting12@live.dk>
2020-06-10 22:10:24 +0200

    Revamps posts to fetch from initial list on server. Adds server-side code for generating post list

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  next(); // Always call next on middleware you want to continue the request after
+});
+
 app.use('/api/posts', (req, res, next) => {
   posts = [
     { id: 'abc8172', title: 'First server side post', content: 'This text is coming from the server' },
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 import { Post } from './post.model';
 
@@ -8,8 +9,15 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts]; // ES6 operator to return the current/original state of the Posts array
+    this.http.get<{message: String, posts: Post[]}>('http://localhost:3000/api/posts')
+    // We don't need to unsubscribe from observables, for features built into Angular (such as the http module)
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts])
+      });
   }
 
   getPostsUpdateListener() {
@@ -17,7 +25,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = {title: title, content: content};
+    const post: Post = { id: null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
