c1db9b00cfbc66eb0b878ce5676f5aa2726db3f3
Martin Aarhus Gregersen <marting12@live.dk>
2020-09-04 10:09:24 +0200

    Adds image upload for posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -27,31 +27,52 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('', multer(storage).single("image"), (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post Added succesfully',
-      postId: createdPost._id
+router.post(
+  '',
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
+    const url = req.protocol + '://' + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename
     });
-  });
-});
+    post.save().then(createdPost => {
+      res.status(201).json({
+        message: 'Post Added succesfully',
+        post: {
+          ...createdPost,
+          id: createdPost._id,
+        }
+      });
+    });
+  }
+);
 
-router.put("/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    res.status(200).json({message: 'Update succesful'})
-  });
-});
+router.put(
+  "/:id",
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + '://' + req.get("host");
+      imagePath = url + "/images/" + req.file.filename
+    }
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath
+    });
+    Post.updateOne({_id: req.params.id}, post).then(result => {
+      res.status(200).json({message: 'Update succesful'})
+    });
+  }
+);
 
-router.get('', (req, res, next) => {
+router.get(
+  '',
+  (req, res, next) => {
   Post.find().then(documents => {
     res.status(200).json({
       message: "Posts fetched succesfully!",
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -12,6 +12,7 @@ import { mimeType } from './mime-type.validator';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
+
   enteredTitle = '';
   enteredContent = '';
   post: Post;
@@ -46,11 +47,13 @@ export class PostCreateComponent implements OnInit {
           this.post = {
             id: postData._id,
             title: postData.title,
-            content: postData.content
+            content: postData.content,
+            imagePath: postData.imagePath
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
+            image: this.post.imagePath
           });
         });
       } else {
@@ -77,12 +80,17 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true; // Doesnt need to be set back to false as we navigate away from the page
     if (this.mode === 'create') {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(
+        this.form.value.title,
+        this.form.value.content,
+        this.form.value.image
+      );
     } else {
       this.postsService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     }
 
